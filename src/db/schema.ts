cc0e09b8cab807b903936aa60ce61d86a39846ec
TongLongDev <tonglong.dev@gmail.com>
2025-12-07 16:42:26 +0700

    database setup

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,12 @@
-import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+import { pgTable, uuid, text, boolean, timestamp } from 'drizzle-orm/pg-core'
 
 export const todos = pgTable('todos', {
-  id: serial('id').primaryKey(),
-  title: text('title').notNull(),
-  createdAt: timestamp('created_at').defaultNow(),
+  id: uuid('id').primaryKey().defaultRandom(),
+  name: text('name').notNull(),
+  isComplete: boolean().notNull(),
+  createdAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
+  updatedAt: timestamp({ withTimezone: true })
+    .defaultNow()
+    .notNull()
+    .$onUpdate(() => new Date()),
 })
